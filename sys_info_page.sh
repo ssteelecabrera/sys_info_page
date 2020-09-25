816d883e68f6747d701791e5e96865c8d08a6b62
ssteelecabrera <steele.sarahruth@gmail.com>
2020-09-25 16:43:43 -0400

    add details for functions

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -4,14 +4,19 @@
 TITLE="System Information Report For $HOSTNAME" CURRENT_TIME="$(date +"%x %r %Z")" TIMESTAMP="Generated $CURRENT_TIME, by $USER"
 
 report_uptime () {
+    echo "Function report_uptime executed."
     return
 
 }
 
-report_disk_space () { return
+report_disk_space () { 
+    echo "Function report_disk_space executed."
+     return
 }
 
-report_home_space () { return
+report_home_space () { 
+     echo "Function report_home_space executed."
+     return
 }
 
 cat << _EOF_
