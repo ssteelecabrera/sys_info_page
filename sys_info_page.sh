e35719e20b487dff3b8655b43cfc95a454d71e51
ssteelecabrera <steele.sarahruth@gmail.com>
2020-09-25 15:01:27 -0400

    Add some data

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -4,11 +4,17 @@
 
 TITLE="System Information Report for $HOSTNAME"
 
-echo "<html>
+CURRENT_TIME="$(date +"%x %r %Z")"
+TIMESTAMP="Generated $CURRENT_TIME, by $USER"
+
+cat<< _EOF_
+echo <html>
         <head>
           <title>$TITLE</title>
         </head>
         <body>
           <h1>$TITLE</h1>
+          <p>$TIMESTAMP</p>
         </body>
-      </html>"
+      </html>
+_EOF_
